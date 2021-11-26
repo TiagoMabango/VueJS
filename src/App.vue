fb0f5d7d3a3f41906c82162b3de72e0ed9d79071
TiagoMabango <tiagomabango@gmail.com>
2021-11-26 11:31:28 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Projecto/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Projecto from './components/Projecto.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Projecto
   }
 }
 </script>
---
+++ src/components/Projecto.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <input type="text" v-model="nome">
+        <p v-once>
+             {{i + 1}} - {{pais.nome}}
+        </p>
+    </div>
+</template>
+
+<script>
+    import { reactive } from 'vue'
+    export default {
+        setup() {
+            const objetos = reactive({
+                paises: [
+                    {id:1, nome : "Angola"},
+                    {id:2, nome : "Brasil"},
+                    {id:3, nome : "Cabo Verde"},
+                    {id:4, nome : "Portugal"}
+                ]
+            })
+
+            return {
+                ...objetos
+            };
+        }
+    }
+</script>
